9a5a94c870e07ff4f3fec1f6c99b272035699ff0
aziz <erazizeee@gmail.com>
2022-09-07 17:07:58 +0530

    Data-type

---
+++ main.js
@@ -0,0 +1,53 @@
+//number
+
+let a = 15;
+let b = 20;
+let c = (a * b);
+
+console.log(c)
+
+//string
+
+var x = "hello";
+var y = "hii";
+
+console.log(x + " was " + y)
+
+//Boolean
+
+var k = 5;
+var j = "5";
+
+console.log(k == j);
+console.log(k === j);
+
+//undefind
+var z;
+console.log(z)
+
+//null
+
+// var i = null;
+// console.log(i);
+
+// Array
+
+// var cars =["Bently",5, red, "Koeggnigns-Agera"]
+// console.log(cars)
+
+//object
+
+
+
+var foods={
+    biryani: 10,
+    parotta: 20
+}
+console.log(foods);
+
+
+const obj=null;
+const obj1={message: "hii!"};
+obj===null;
+obj===null;
+console.log(obj1);
